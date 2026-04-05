1a156a6f4ef0a78afcb2db3ec04b4018e0504a6c
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-04-05 17:14:40 +0200

    feat: added Hero class, closes #2

---
+++ models/AHero.cs
@@ -0,0 +1,23 @@
+﻿namespace BLOC3.PP7_HeroEngine.models;
+
+public abstract class AHero
+{
+    protected string Name { get; set; }
+    protected int Level { get; set; }
+    protected int CurrentHp { get; set; }
+    protected int MaxHp { get; set; }
+    protected int Power { get; set; }
+
+    protected AHero(string name, int level, int currentHp, int maxHp, int power)
+    {
+        Name = name;
+        Level = level;
+        CurrentHp = currentHp;
+        MaxHp = maxHp;
+        Power = power;
+    }
+
+    public abstract string Greeting();
+    public abstract int Attack();
+    public abstract int TakeDamage(int damage);
+}
